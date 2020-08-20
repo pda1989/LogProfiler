a49bc5b2eeb2e2ef585bd5c345a5b3bffd6c4526
Dmitry Pokhlebaev <pokhlebaev.dmitry@gmail.com>
2020-08-20 23:13:52 +0200

    inject ILogger into LogProfiler

--- LogProfiler/Implementations/LogProfiler.cs
+++ LogProfiler/Implementations/LogProfiler.cs
@@ -1,38 +1,42 @@
-﻿using LogProfiler.Interfaces;
+﻿using LogProfiler.Enumerations;
+using LogProfiler.Interfaces;
 
 namespace LogProfiler.Implementations
 {
     public class LogProfiler : ILogProfiler
     {
+        private readonly ILogger _logger;
+
         public static ILogProfilerFactory Factory => new LogProfilerFactory();
 
-        public LogProfiler()
+        public LogProfiler(ILogger logger)
         {
+            _logger = logger;
         }
 
         public void Debug(string message, params string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.Write(LogLevel.Debug, message, args);
         }
 
         public void Error(string message, params string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.Write(LogLevel.Error, message, args);
         }
 
         public void Information(string message, params string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.Write(LogLevel.Information, message, args);
         }
 
         public void Verbose(string message, params string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.Write(LogLevel.Verbose, message, args);
         }
 
         public void Warning(string message, params string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.Write(LogLevel.Warning, message, args);
         }
     }
 }
